8de8e905d3f86401958d753ba3536d0e59251699
Shyam <shyam.sapate@cuelogic.co.in>
2017-02-08 14:35:43 +0530

    Angular Js Training Demo Project

---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+eventsApp.controller('EditEventController',
+	function EditEventController($scope, eventData) {
+
+		$scope.event = {};
+
+		$scope.saveEvent = function(event, newEventForm) {
+			if(newEventForm.$valid) {
+				eventData.save(event)
+					.$promise
+					.then(function(response) { console.log('success', response) })
+					.catch(function(response) { console.log('failure', response) });
+			}
+		};
+
+		$scope.cancelEdit = function() {
+			window.location = "/EventDetails.html";
+		};
+	}
+);
