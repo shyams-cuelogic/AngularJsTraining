b1199056ddd320991ef100744426b3a567d5cd1b
Shyam <shyam.sapate@cuelogic.co.in>
2017-02-09 15:55:54 +0530

    Creating Directives

---
+++ app/js/directives/eventThumbnail.js
@@ -0,0 +1,13 @@
+'use strict';
+
+eventsApp.directive('eventThumbnail', function($compile) {
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: 'templates/directives/eventThumbnail.html',
+		scope: {
+			event: "="
+		/*	event: "=myEvent" */
+		}
+	}
+});
