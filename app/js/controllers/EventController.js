31e4067ff8d878943f9edd8ef791e01f42c0d0ab
Shyam <shyam.sapate@cuelogic.co.in>
2017-02-09 11:29:45 +0530

    Practicals on Routing

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,13 +1,19 @@
 'use strict';
 
 eventsApp.controller('EventController',
-	function EventController($scope, eventData, $anchorScroll) {
+	function EventController($scope, eventData, $anchorScroll, $routeParams, $route) {
 		$scope.sortorder = 'name';
-		eventData.getEvent()
+		$scope.event = $route.current.locals.event;
+/*		eventData.getEvent($routeParams.eventId)
 			.$promise.then(
 				function(event) { $scope.event = event; console.log(event); },
 				function(response) { console.log(response); }
-			);
+			);*/
+		console.log($route.current.params.hi);
+
+		$scope.reload = function() {
+			$route.reload();
+		}
 
 		$scope.upVoteSession = function(session) {
 			session.upVoteCount++;
