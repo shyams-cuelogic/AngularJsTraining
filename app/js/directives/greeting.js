d151aef2d59e3b6a05e0e57e3c2936d311b8b383
Shyam <shyam.sapate@cuelogic.co.in>
2017-02-09 18:55:09 +0530

    Adding Plugins using Directive

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -1,18 +1,38 @@
 'use strict';
 
-eventsApp.directive('greeting', function() {
-	return {
-		restrict: 'E',
-		replace: true,
-		template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
-		controller: '@',
-		name: 'ctrl'
-	};
-});
-
-eventsApp.controller('GreetingController',
-	function GreetingController($scope) {
-		$scope.sayHello = function() {
-			alert('Hello');
+eventsApp.
+	directive('greeting', function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			transclude: true,
+			template: "<div><button class='btn' ng-click='sayHello()'>Say Hello</button><div ng-transclude>",
+			controller: function($scope) {
+				var greetings = ['hello'];
+				$scope.sayHello = function() {
+					alert(greetings.join());
+				}
+				this.addGreeting = function(greeting) {
+					greetings.push(greeting);
+				}
+			}
+		};
+	})
+	.directive('finnish', function() {
+		return {
+			restrict: 'A',
+			require: '^greeting',
+			link: function(scope, element, attrs, controller) {
+				controller.addGreeting('Hei');
+			}
+		}
+	})
+	.directive('hindi', function() {
+		return {
+			restrict: 'A',
+			require: '^greeting',
+			link: function(scope, element, attrs, controller) {
+				controller.addGreeting('नमस्ते');
+			}
 		}
 	});
